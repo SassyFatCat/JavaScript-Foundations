a51537c1b563d8e9645085d0c743d1396a1fa3e0
Ryan Lee <therealsassyfatcat@outlook.com>
2020-06-08 21:11:08 -0500

    got the first part working

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
-
+let principal = 200000;
+let interestRate = 0.05;
+let years = 30;
+let name = "Ryan";
 
 
 
@@ -14,7 +17,8 @@
 (1) Create a variable called `monthlyInterestRate` and give it the value of interest rate divided by 12. 
 (2) Create another variable called `periods` and give it the value of years*12.
 */
-
+let monthlyInterestRate = interestRate/12;
+let periods = years*12;
 
 
 
@@ -36,8 +40,11 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 When your math is correct, monthlyRate will equal 1073.64
 */
 
+let n1 = Math.pow(monthlyInterestRate + 1, periods);
+let numerator = monthlyInterestRate*n1;
+let denominator = n1 - 1;
 
-
+let monthlyRate = principal*(numerator/denominator);
 
 // 🏡 Task 3: Function
 /* Create a function called `mortgageCalculator` that combines all of the steps from task 1 and 2 and returns a sentence "{Name}, your monthly rate is ${monthlyRate}"
@@ -45,7 +52,9 @@ When your math is correct, monthlyRate will equal 1073.64
 If your name is `Oscar` mortgageCalculator() should return "Oscar, your monthly rate is 1073.64"
 */
 
-
+let mortgageCalculator = () => {
+    return `${name}, your monthly rate is ${monthlyRate}`;
+}
 
 
 
